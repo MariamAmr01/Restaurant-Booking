11eea4cef8508efc23a7273179b90198e7acaba0
Mariam Amr <a.mariamgharieb@gmail.com>
2022-06-07 06:40:53 +0200

    Add files via upload

---
+++ scriptHome.js
@@ -0,0 +1,11 @@
+function showInfo() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+       document.getElementById("learn").innerText = this.responseText;
+      }
+    };
+    xhttp.open("GET", "learn.html", true);
+    xhttp.send();
+  }
+  
